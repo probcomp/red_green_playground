8febb57dee200fed505676598e1d166826d42a04
Arijit Dasgupta <arijit.dg@hotmail.com>
2025-06-15 21:12:34 +1000

    update flask port

--- create_stimuli.py
+++ create_stimuli.py
@@ -511,4 +511,5 @@ def clear_simulation():
     return jsonify({"status": "success", "message": "Simulation cleared."})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
