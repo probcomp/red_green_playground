f585b20f2d15d477b57603f7aa247ef14897c0d2
Arijit Dasgupta <arijit.dg@hotmail.com>
2025-06-15 20:51:23 +1000

    update, local deployment working

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+// Handle React routing, return all requests to React app
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
